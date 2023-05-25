6403454ba093e6a6aa65a30d970339ba4fa57d0a
Arnaldo Barbosa <arnaldotenorio044@gmail.com>
2023-05-25 17:08:17 -0300

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  resources :lists, only: %i{create new destroy show} do
+    resources :bookmarks, only: %i{create new}
+  end
+  resources :bookmarks, only: :destroy
+  root "list#index"
+  # get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # root to: "pages#home"
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
